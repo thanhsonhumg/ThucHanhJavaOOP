ebf7a11579f6bc1a22008c92db3c64f7f61f88ce
thanhsonhumg <son29102k2@gmail.com>
2022-02-19 12:19:16 +0700

    first commit

--- BaiThucHanhJava01/src/App.java
+++ BaiThucHanhJava01/src/App.java
@@ -2,6 +2,6 @@ package BaiThucHanhJava.BaiThucHanhJava01.src;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        System.out.println("Hello, World !");
     }
 }
