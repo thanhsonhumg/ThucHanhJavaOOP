a1f1f986ad462c4e763f4477e6b80b10162d242f
thanhsonhumg <son29102k2@gmail.com>
2022-02-26 18:04:12 +0700

    25/02

--- BaiThucHanhJava2502/App11.java
+++ BaiThucHanhJava2502/App11.java
@@ -3,7 +3,7 @@ public class App11 {
         int i,j,k;
         System.out.println("Cac phuong an co the de co tong 200000 tu 3 loai giay bac 10 000 dong, 20 000 dong va 50 000 dong la: ");
             for(i=0;i<=4;i++)
-                for(j=0;j<=10;j++)
+                for( j=0;j<=10;j++ )
                     for(k=0;k<=20;k++)
                         if((i*50000+j*20000+k*10000)==200000)
                             System.out.println(k+" to 10 000 dong + "+ j + " to 20 000 dong + " + i +" to 50 000 dong.");
